4cbddf8e45bd426ce5b398e1c942c01703f4df83
Guy Irvine <guy@guyirvine.com>
2011-12-23 22:21:43 +1300

    Added UI functionality for creating and updating notes and cards

--- card-board.js
+++ card-board.js
@@ -1,67 +1,50 @@
-var cardId = 0;
-var noteId = 0;
+function addInteraction( id ) {
+	$( id ).draggable();
+	$( id ).mouseover( function() {
+		$( id ).addClass( "highlight" ) })
+		.mouseout( function() {
+		$( id ).removeClass( "highlight" ) })
+	;
+}
 
-	function addInteraction( id ) {
-		$( id ).draggable();
-		$( id ).mouseover( function() {
-			$( id ).addClass( "highlight" ) })
-			.mouseout( function() {
-			$( id ).removeClass( "highlight" ) })
-			;
+function updateTips( tipControl, t ) {
+	tipControl
+		.text( t )
+		.addClass( "ui-state-highlight" );
+	setTimeout(function() {
+		tipControl.removeClass( "ui-state-highlight", 1500 );
+	}, 500 );
+}
+
+function checkLength( tipControl, o, n, min, max ) {
+	if ( o.val().length > max || o.val().length < min ) {
+		o.addClass( "ui-state-error" );
+		updateTips( tipControl, "Length of " + n + " must be between " +
+			min + " and " + max + "." );
+		return false;
+	} else {
+		return true;
 	}
+}
 
-	function addCard( id, top, left, title, description ) {
-		cardId++;
-		string = "" +
-				"<div id='card-" + cardId + "' class='card shadow' style='top: " + top + "px;left: " + left + "px;'>" +
-					"<div class='line' style='top: 25px;'>&nbsp;</div>" +
-					"<div class='line' style='top: 41px;'>&nbsp;</div>" +
-					"<div class='line' style='top: 57px;'>&nbsp;</div>" +
-					"<div class='line' style='top: 73px;'>&nbsp;</div>" +
-					"<div class='line' style='top: 89px;'>&nbsp;</div>" +
-					"<div class='line' style='top: 105px;'>&nbsp;</div>" +
-					"<div class='title'>" + title + "</div>" +
-					"<div class='body'>" + description + "</div>" +
-					"</div>";
-
-		$( '#card-layout' ).append( string );
-		addInteraction( "#card-" + cardId );
+function checkRegexp( tipControl, o, regexp, n ) {
+	if ( !( regexp.test( o.val() ) ) ) {
+		o.addClass( "ui-state-error" );
+		updateTips( tipControl, n );
+		return false;
+	} else {
+		return true;
 	}
+}
 
-	function addNote( id, top, left, description ) {
-		noteId++;
-		string = "" +
-				"<div id='note-" + noteId + "' class='note shadow' style='top: " + top + "px;left: " + left + "px;'>" +
-					description +
-					"</div>";
 
-		$( '#card-layout' ).append( string );
-		addInteraction( "#note-" + noteId );
-	}
+/*********************************************************************************************/
+/* Card */
+var nextCardId = 0;
+var currentCardId = null;
 
-		function checkLength( o, n, min, max ) {
-			if ( o.val().length > max || o.val().length < min ) {
-				o.addClass( "ui-state-error" );
-				updateTips( "Length of " + n + " must be between " +
-					min + " and " + max + "." );
-				return false;
-			} else {
-				return true;
-			}
-		}
-
-		function checkRegexp( o, regexp, n ) {
-			if ( !( regexp.test( o.val() ) ) ) {
-				o.addClass( "ui-state-error" );
-				updateTips( n );
-				return false;
-			} else {
-				return true;
-			}
-		}
-
-		function createDialogs() {
-$( "#dialog-form-card" ).dialog({
+function createCardDialog() {
+	$( "#dialog-form-card" ).dialog({
 			autoOpen: false,
 			height: 300,
 			width: 350,
@@ -69,16 +52,22 @@ $( "#dialog-form-card" ).dialog({
 			buttons: {
 				"Create a card": function() {
 					var bValid = true;
-					var allFields = $( [] ).add( title ).add( description )
-					var title = $("#title");
-					var description = $("#description");
+					var cardTitle = $("#card-title");
+					var cardDescription = $("#card-description");
+					var allFields = $( [] ).add( cardTitle ).add( cardDescription )
 					allFields.removeClass( "ui-state-error" );
 
-					bValid = bValid && checkLength( title, "title", 3, 16 );
-					bValid = bValid && checkRegexp( title, /^[a-z]([0-9a-z_])+$/i, "Title may consist of a-z, 0-9, underscores, begin with a letter." );
+					bValid = bValid && checkLength( $( ".validateCardTips" ), cardTitle, "card-title", 3, 16 );
+					bValid = bValid && checkRegexp( $( ".validateCardTips" ), cardTitle, /^[a-z]([0-9a-z_ ])+$/i, "Title may consist of a-z, 0-9, underscores, begin with a letter." );
 
 					if ( bValid ) {
-						addCard( 4, 250, 250, title.val(), description.val() );
+						if ( currentCardId == null ) {
+							addCard( 4, 250, 250, cardTitle.val(), cardDescription.val() );
+						} else {
+							$("#card-" + currentCardId + "-title").text($( "#card-title" ).val() );
+							$("#card-" + currentCardId + "-description").text($( "#card-description" ).val( ) );	
+						}
+							
 						$( this ).dialog( "close" );
 					}
 				},
@@ -87,12 +76,50 @@ $( "#dialog-form-card" ).dialog({
 				}
 			},
 			close: function() {
-		var allFields = $( [] ).add( title ).add( description )
+				var cardTitle = $("#card-title");
+				var cardDescription = $("#card-description");
+				var allFields = $( [] ).add( cardTitle ).add( cardDescription )
 				allFields.val( "" ).removeClass( "ui-state-error" );
 			}
 		});
+}	
 
-$( "#dialog-form-note" ).dialog({
+function addCard( id, top, left, title, description ) {
+	var cardId = nextCardId++;
+	string = "" +
+			"<div id='card-" + cardId + "' class='card shadow' style='top: " + top + "px;left: " + left + "px;'>" +
+				"<div class='line' style='top: 25px;'>&nbsp;</div>" +
+				"<div class='line' style='top: 41px;'>&nbsp;</div>" +
+				"<div class='line' style='top: 57px;'>&nbsp;</div>" +
+				"<div class='line' style='top: 73px;'>&nbsp;</div>" +
+				"<div class='line' style='top: 89px;'>&nbsp;</div>" +
+				"<div class='line' style='top: 105px;'>&nbsp;</div>" +
+				"<div class='title' id='card-" + cardId + "-title'>" + title + "</div>" +
+				"<div class='body' id='card-" + cardId + "-description'>" + description + "</div>" +
+				"</div>";
+
+	$( '#card-layout' ).append( string );
+	addInteraction( "#card-" + cardId );
+
+	$( "#card-" + cardId )
+				.click(function() {
+					currentCardId = cardId;
+					$( "#card-title" ).val( $("#card-" + cardId + "-title").text() );	
+					$( "#card-description" ).val( $("#card-" + cardId + "-description").text() );	
+					$( "#ui-dialog-title-dialog-form-card" ).text( "Update Card" );
+					$( "#dialog-form-card" ).dialog( "open" );
+				});
+}
+
+/*********************************************************************************************/
+
+/*********************************************************************************************/
+/* Note */
+var nextNoteId = 0;
+var currentNoteId = null;
+
+function createNoteDialog() {
+	$( "#dialog-form-note" ).dialog({
 			autoOpen: false,
 			height: 300,
 			width: 350,
@@ -104,10 +131,14 @@ $( "#dialog-form-note" ).dialog({
 					var allFields = $( [] ).add( noteDescription )
 					allFields.removeClass( "ui-state-error" );
 
-					bValid = bValid && checkLength( noteDescription, "note-description", 3, 16 );
+					bValid = bValid && checkLength( $( ".validateNoteTips" ), noteDescription, "note-description", 1, 2000 );
 
 					if ( bValid ) {
-						addNote( 4, 250, 400, noteDescription.val() );
+						if ( currentNoteId == null ) {
+							addNote( 4, 250, 400, noteDescription.val() );
+						} else {
+							$("#note-" + currentNoteId).text( noteDescription.val() );
+						}
 						$( this ).dialog( "close" );
 					}
 				},
@@ -117,20 +148,47 @@ $( "#dialog-form-note" ).dialog({
 			},
 			close: function() {
 				var noteDescription = $("#note-description");
-				var allFields = $( [] ).add( title ).add( noteDescription )
+				var allFields = $( [] ).add( noteDescription )
 				allFields.val( "" ).removeClass( "ui-state-error" );
 			}
 		});
+	}
+	
+function addNote( id, top, left, description ) {
+	var noteId = nextNoteId++;
+	string = "" +
+			"<div id='note-" + noteId + "' class='note shadow' style='top: " + top + "px;left: " + left + "px;'>" +
+				description +
+				"</div>";
 
-		$( "#create-card" )
-					.button()
-					.click(function() {
-						$( "#dialog-form-card" ).dialog( "open" );
-					});
-		$( "#create-note" )
-					.button()
-					.click(function() {
-						$( "#dialog-form-note" ).dialog( "open" );
-					});
+	$( '#card-layout' ).append( string );
+	addInteraction( "#note-" + noteId );
+
+	$( "#note-" + noteId )
+				.click(function() {
+					currentNoteId = noteId;
+					$( "#note-description" ).val( $("#note-" + noteId).text() );	
+					$( "#ui-dialog-title-dialog-form-note" ).text( "Update Note" );
+					$( "#dialog-form-note" ).dialog( "open" );
+				});
+}
+/*********************************************************************************************/
+function createDialogs() {
+	createCardDialog();
+	createNoteDialog();
+
+	$( "#create-card" )
+				.button()
+				.click(function() {
+					$( "#ui-dialog-title-dialog-form-card" ).text( "Create a Card" );
+					$( "#dialog-form-card" ).dialog( "open" );
+				});
+	$( "#create-note" )
+				.button()
+				.click(function() {
+					currentNoteId = null;
+					$( "#ui-dialog-title-dialog-form-note" ).text( "Create a Note" );
+					$( "#dialog-form-note" ).dialog( "open" );
+				});
 	}
 	
