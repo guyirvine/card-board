93dcdd2159ebfffe1f755a747f7c0a531da1c335
Guy Irvine <guy@guyirvine.com>
2011-12-24 20:13:58 +1300

    Removed unused files

--- app.js
+++ app.js
@@ -13,18 +13,14 @@ DELETE  /forums/:forum       ->  destroy
 */
 
 var express = require('express')
-  , routes = require('./routes')
   , Resource = require('express-resource')
   , app = module.exports = express.createServer();
 
 // Configuration
 
 app.configure(function(){
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
   app.use(express.bodyParser());
   app.use(express.methodOverride());
-  app.use(app.router);
   app.use(express.static(__dirname + '/public'));
 });
 
@@ -37,26 +33,6 @@ app.configure('production', function(){
 });
 
 // Routes
-/*
-app.post('/msg.htm', function( request, res ) {
-	console.log( "POSTDATA: " + request.body.description );
-	
-        var body = '';
-        request.on('data', function (data) {
-            body += data;
-        });
-        request.on('end', function () {
-		var obj = JSON.parse(body);
-		console.log( "POSTDATA: " + obj.description );
-            // use POST
-
-        });
-    
-	res.send( "Fling" );
-});
-*/
-
-app.get('/', routes.index);
 app.resource('note', require('./routes/note'));
 
 app.listen(3000);
