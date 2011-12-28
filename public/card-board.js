d43191f7d12ac8c77da29abc175d3b1ebf5e88b2
Guy Irvine <guy@guyirvine.com>
2011-12-29 11:45:43 +1300

    Got document id from querystring

--- public/card-board.js
+++ public/card-board.js
@@ -1,5 +1,20 @@
 /*********************************************************************************************/
 /* UI Helpers */
+// Read a page's GET URL variables and return them as an associative array.
+function getUrlVars()
+{
+    var vars = [], hash;
+    var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+    for(var i = 0; i < hashes.length; i++)
+    {
+        hash = hashes[i].split('=');
+        vars.push(hash[0]);
+        vars[hash[0]] = hash[1];
+    }
+    return vars;
+}
+
+
 function addHighlight( id ) {
 	$( id ).mouseover( function() {
 		$( id ).addClass( "highlight" ) })
