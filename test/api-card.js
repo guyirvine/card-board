bd7537908e712cbadb95ccd224c7dd207383e3ca
Guy Irvine <guy@guyirvine.com>
2011-12-31 08:59:04 +1300

    Added vows(BDD) testing

---
+++ test/api-card.js
@@ -0,0 +1,46 @@
+/*
+ * INSTRUCTIONS
+ *
+ * run the site at localhost, port 3000
+ *
+ * run vows --spec test/api-card.js
+ *
+ */
+
+var request = require('request'),
+    vows = require('vows'),
+    assert = require('assert'),
+    cookie = null,
+    common = require('test-api-common')
+
+
+// TODO include unauthed tests
+var suite = vows.describe('API Localhost HTTP Tests')
+
+// Very first test!
+.addBatch({
+  "Server should be UP as in: var apiUrl": {
+    topic: function(){
+      common.get('', {} ,this.callback )
+    },
+
+    '/ should repond something' : function(res, b){
+      assert.ok(res.body)
+    }
+  }
+})
+
+
+.addBatch({
+  'card#index': {
+    topic: function(){
+      common.get('card', {}, this.callback)
+    },
+    'should be 200': common.assertStatus(200),
+    'should have JSON header' : common.assertJSONHead(),
+    'body is valid JSON' : common.assertValidJSON(),
+  },
+})
+
+//suite.run( )
+suite.export( module )
---
+++ test/api-note.js
@@ -0,0 +1,71 @@
+/*
+ * INSTRUCTIONS
+ *
+ * run the site at localhost, port 3000
+ *
+ * run vows --spec test/api-note.js
+ *
+ */
+
+
+var request = require('request'),
+    vows = require('vows'),
+    assert = require('assert'),
+    cookie = null,
+    common = require('test-api-common')
+
+// TODO include unauthed tests
+var suite = vows.describe('API Localhost HTTP Tests')
+
+// Very first test!
+.addBatch({
+  "Server should be UP as in: var apiUrl": {
+    topic: function(){
+      common.get('', {} ,this.callback )
+    },
+
+    '/ should repond something' : function(res, b){
+      assert.ok(res.body)
+    }
+  }
+})
+
+/*
+.addBatch(
+{
+  'Authenticate to /login': {
+    topic: function(){
+      request.post(
+        {
+          url: "http://localhost:8010/login",
+          json: { user:{ username: 'flockin_lab', password: '123456' }}
+        },
+        this.callback
+      );
+    },
+
+    'get a valid Cookie': function(req, res, body, err){
+      try{
+        cookie = res.headers['set-cookie'].pop().split(';')[0]
+        console.log("GOT COOKIE!", cookie)
+      } catch(e){ }
+
+      assert.ok( typeof(cookie) == 'string' && cookie.length > 10 )
+    }
+})
+*/
+
+
+.addBatch({
+  'note#index': {
+    topic: function(){
+      common.get('note', {}, this.callback)
+    },
+    'should be 200': common.assertStatus(200),
+    'should have JSON header' : common.assertJSONHead(),
+    'body is valid JSON' : common.assertValidJSON(),
+  },
+})
+
+//suite.run( )
+suite.export( module )
